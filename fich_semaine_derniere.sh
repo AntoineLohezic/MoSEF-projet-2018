e1ab9f1036dc8d6a45622038b4244ab62b9f62a1
Antoine Lohézic <lohezic.antoine@gmail.com>
2018-11-22 17:14:35 +0100

    Upgraded Amadou's Project

--- fich_semaine_derniere.sh
+++ fich_semaine_derniere.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-date=$(date)
-echo "Bienvenue $USERNAME, nous sommes le $date"
+echo "Bienvenue $USERNAME, nous sommes le $(date)"
 echo "Voici votre HOME :"
 cd ~
 ls
@@ -8,3 +7,5 @@ read -p "Quel répertoire vous intéresse aujourd'hui ? " rep
 cd $rep
 echo "Dans $rep il y a :"
 ls
+echo "Les fichiers modifiés lors des sept derniers jours sont :"
+echo $(find -type f -mtime -7)
